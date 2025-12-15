c54f1cbaf42deb980878b652d42551ef74dcebf0
hastingsomari <hastingsomari@gmail.com>
2025-12-15 14:36:26 +0300

    analyzing data module syntax

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+
+    for (let arr of args) {
+        combinedObject.users = [...combinedObject.users, ...arr];
+    }
+
+    combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+    return combinedObject;
+}
+
+module.exports = { combineUsers };
 
 
 module.exports = {
